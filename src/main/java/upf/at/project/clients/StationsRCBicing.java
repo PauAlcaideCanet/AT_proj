583195d6632a17040659cfd5b59d7e1ce936f0b1
AlbertGD14 <albert.garcia13@estudiant.upf.edu>
2025-02-21 13:49:31 +0100

    cache finished

--- src/main/java/upf/at/project/clients/StationsRCBicing.java
+++ src/main/java/upf/at/project/clients/StationsRCBicing.java
@@ -18,11 +18,12 @@ public class StationsRCBicing {
 		Client client = ClientBuilder.newClient();
 		WebTarget targetGet = client.target("https://opendata-ajuntament.barcelona.cat")
 				.path("/data/dataset/6aa3416d-ce1a-494d-861b7bd07f069600/resource/1b215493-9e63-4a12-8980-2d7e0fa19f85/download");
-		//if(data == null || (new Date().getTime() - lastUpdateTime.getTime()) > TIMEOUT) {
-		data = targetGet.request(MediaType.APPLICATION_JSON_TYPE).
-				header("Authorization", "5fbf9a6fa8f1b0fba915e7891a4b5aa807e6c257520cb8d5ad5e3dfc9846677a")
-				.get(new GenericType<Data>() {});
-		//}
+		if(data == null || (new Date().getTime() - lastUpdateTime.getTime()) > TIMEOUT) {
+			data = targetGet.request(MediaType.APPLICATION_JSON_TYPE).
+					header("Authorization", "5fbf9a6fa8f1b0fba915e7891a4b5aa807e6c257520cb8d5ad5e3dfc9846677a")
+					.get(new GenericType<Data>() {});
+			lastUpdateTime = new Date();
+		}
 		
 		return data;
 		
