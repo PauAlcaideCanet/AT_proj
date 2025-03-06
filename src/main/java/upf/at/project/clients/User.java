4f52b5aae057a84a6d34a5e71c92ce16289e9c8b
PauAlcaideCanet <pau.alcaide01@estudiant.upf.edu>
2025-03-06 19:05:23 +0100

    Modifications on the toString User

--- src/main/java/upf/at/project/clients/User.java
+++ src/main/java/upf/at/project/clients/User.java
@@ -47,6 +47,6 @@ public class User {
 	//toString
 	@Override
 	public String toString() {
-		return "Client [phone=" + phone + ", subStationsId=" + subStationsId + ", telegramToken=" + telegramToken + "]";
+		return "\"Client\" : [phone=" + phone + ", subStationsId=" + subStationsId + ", telegramToken=" + telegramToken + "]";
 	}
 }
